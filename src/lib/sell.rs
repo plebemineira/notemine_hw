4b142d377b7d6c049656129df94b1f3f6bb3f56b
plebhash <plebhash@proton.me>
2024-09-20 21:48:51 -0300

    refine verify_zap
    Signed-off-by: plebhash <plebhash@proton.me>

--- src/lib/sell.rs
+++ src/lib/sell.rs
@@ -2,7 +2,13 @@ pub fn pow_price(pow_price_factor: f64, difficulty: u32) -> f64 {
     2f64.powf(pow_price_factor * (difficulty as f64))
 }
 
-pub async fn verify_zap(_zap: String, _difficulty: u32) -> bool {
-    // todo
+pub async fn verify_zap(_zap: String, pow_price_factor: f64, difficulty: u32) -> bool {
+    let _pow_price = pow_price(pow_price_factor, difficulty);
+
+    // todo: check zap is valid
+
+    // let zap_value = 100000000; // todo: read zap value
+    // return (zap_value as f64) > pow_price
+
     true
 }
